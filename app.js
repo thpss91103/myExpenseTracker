650447e7ef1fbca1889e11b34e492c17346e2bb7
thpss91103 <a0975142932@gmail.com>
2023-07-03 22:46:03 +0800

    feat: add signup page

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const routes = require('./routes')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const flash = require("connect-flash")
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-
+app.use(flash())
 
 app.use(routes)
 
 // start and listen on the Express server
-app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is listening on localhost:${PORT}`)
 })
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-
+router.get('/signup', (req, res) => res.render('signup'))
 router.get('/signin', (req, res) => res.render('signin'))
 
 router.get('/', (req, res) => res.redirect('/signin'))
